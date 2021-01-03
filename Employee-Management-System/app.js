1757311af6db212fc341b03d9388642ea96ca2d5
Your Name <suttonk@bcglobal.net>
2021-01-02 17:42:24 -0800

    added required app fuctionality

--- Employee-Management-System/app.js
+++ Employee-Management-System/app.js
@@ -17,14 +17,15 @@ connection.connect(function(err){
     employeeNavbar()
 })
 
-
 function employeeNavbar() {
     inquirer.prompt([
         {
             name: "userresponse",
             type: "list",
             message: "What would you like to do?",
-            choices: ["View Department", "View Employee", "View Role", "Exit APP"]
+            choices: ["View Department", "View Employee", "View Role",
+                      "Add Department", /*"Add Employee", "Add Role",*/
+                      "Exit APP"]
         }
     ]).then(function (data) {
         switch (data.userresponse) {
@@ -37,13 +38,73 @@ function employeeNavbar() {
             case "View Role":
                 displayRole();
                 break;
+            case "Add Department":
+                inquirer.prompt([
+                {
+                    name: "userresponse",
+                    type: "list",
+                    message: "What department would you like to add?",
+                    choices: ["Sales", "Public Relations",
+                              "Human Resources", "Quality Assurance",
+                              "Research & Development",
+                              "Exit APP"]
+                }
+                ]).then(function (data) {
+                    switch (data.userresponse) {
+                        case "Sales":
+                            addDepartment("Sales");
+                            break;
+                        case "Public Relations":
+                            addDepartment("Public Relations");
+                            break;
+                        case "Human Resources":
+                            addDepartment("Human Resources");
+                            break;
+                        case "Quality Assurance":
+                            addDepartment("Quality Assurance");
+                            break;
+                        case "Research & Development":
+                            addDepartment("Research & Development");
+                            break;
+                        default:
+                            break;                
+                    }
+            })              
+                break;
+            /*
+            case "Add Employee":
+                inquirer.prompt([
+                {
+                    name: "userresponse",
+                    type: "string",
+                    message: "Enter Employee Info, ",
+                    message: "(FirstName LastName RoleID ManagerID): ",
+                }
+                ])/.then(function (data) {
+                    addEmployee(data.userresponse);
+                }    
+            )    
+                break;              
+            case "Add Role":
+                addRole();
+                break;    
+            */              
             default:
                 process.exit(0);
-        }
+}
 })  }
 
 function displayDepartment(){
     connection.query("SELECT * FROM DEPARTMENT;",function(error,data){
+        if ( error) throw error
+        console.table(data)
+        employeeNavbar()
+    })
+}
+
+function addDepartment(req, res){
+    //console.log("Add Department: ", req)
+    connection.query("INSERT INTO DEPARTMENT (name) values (?)", [req], function(error, data) {
         if ( error) throw error
         console.table(data)
         employeeNavbar()
@@ -57,6 +118,17 @@ function displayEmployee(){
         employeeNavbar()
     })
 }
+
+function addEmployee(req, res){
+    //console.log("Add Employee: ", req)
+    connection.query("INSERT INTO EMPLOYEE (First Name, Last Name, Employee ID, Manager ID) values = ?", [req], function(error, data) {
+        if ( error) throw error
+        console.table(data)
+        employeeNavbar()
+    })
+}
+
+
 function displayRole(){
     connection.query("SELECT * FROM ROLE;",function(error,data){
         if ( error) throw error
